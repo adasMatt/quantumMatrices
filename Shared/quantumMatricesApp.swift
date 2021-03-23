fc849a7b4d36e12d9ec56822bcb8e9f2d360b144
Matt <madas@hawk.iit.edu>
2021-03-23 12:27:53 -0500

    Initial Commit

---
+++ Shared/quantumMatricesApp.swift
@@ -0,0 +1,17 @@
+//
+//  quantumMatricesApp.swift
+//  Shared
+//
+//  Created by Matthew Adas on 3/23/21.
+//
+
+import SwiftUI
+
+@main
+struct quantumMatricesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
